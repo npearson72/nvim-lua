e3ae0f7f8974c9d073cd445a12d0ed45ffe82598
Nathan Pearson <npearson72@gmail.com>
2022-12-26 16:38:02 -0700

    Enhances telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,20 +1,31 @@
 local actions = require('telescope.actions')
+local action_layout = require("telescope.actions.layout")
 local builtin = require('telescope.builtin')
 
 require('telescope').setup{
   defaults = {
     sorting_strategy = "ascending",
+    scroll_strategy = "limit",
     layout_config = {
       prompt_position = "top"
     },
     mappings = {
+      n = {
+        ["<c-p>"] = action_layout.toggle_preview
+      },
       i = {
         ["<esc>"] = actions.close,
         ["<c-j>"] = actions.move_selection_next,
         ["<c-k>"] = actions.move_selection_previous,
+        ["<c-p>"] = action_layout.toggle_preview,
         ["<c-h>"] = "which_key"
       }
     }
+  },
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+    },
   }
 }
 
