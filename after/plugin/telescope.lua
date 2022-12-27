8afde9b66fc502856a867cf6b6fabd5b8e586649
Nathan Pearson <npearson72@gmail.com>
2022-12-27 03:04:40 -0700

    Enhances telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,50 @@
 local actions = require('telescope.actions')
 local action_layout = require("telescope.actions.layout")
+local action_state = require("telescope.actions.state")
 local builtin = require('telescope.builtin')
+
+pickers = {
+  builtin.find_files,
+  builtin.oldfiles,
+  index = 1,
+}
+
+pickers.cycle = function()
+  if pickers.index >= #pickers then
+    pickers.index = 1
+  else
+    pickers.index = pickers.index + 1
+  end
+  pickers[pickers.index]({
+    default_text = action_state.get_current_line()
+  })
+end
+
+
+pickers.close = function(prompt_bufnr)
+  pickers.index = 1
+  actions.close(prompt_bufnr)
+end
+
+pickers.select_default = function(prompt_bufnr)
+  pickers.index = 1
+  actions.select_default(prompt_bufnr, "default")
+end
+
+pickers.select_tab = function(prompt_bufnr)
+  pickers.index = 1
+  actions.select_tab(prompt_bufnr, "default")
+end
+
+pickers.select_vertical = function(prompt_bufnr)
+  pickers.index = 1
+  actions.select_vertical(prompt_bufnr, "default")
+end
+
+pickers.select_horizontal = function(prompt_bufnr)
+  pickers.index = 1
+  actions.select_horizontal(prompt_bufnr, "default")
+end
 
 require('telescope').setup{
   defaults = {
@@ -14,7 +58,36 @@ require('telescope').setup{
         ["<c-p>"] = action_layout.toggle_preview
       },
       i = {
-        ["<esc>"] = actions.close,
+        ["<c-f>"] = {
+          pickers.cycle,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<esc>"] = {
+          pickers.close,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<cr>"] = {
+          pickers.select_default,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<c-t>"] = {
+          pickers.select_tab,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<c-v>"] = {
+          pickers.select_vertical,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<c-s>"] = {
+          pickers.select_horizontal,
+          type = "action",
+          opts = { nowait = true, silent = true }
+        },
         ["<c-j>"] = actions.move_selection_next,
         ["<c-k>"] = actions.move_selection_previous,
         ["<c-p>"] = action_layout.toggle_preview,
