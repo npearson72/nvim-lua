8cb436df961bbc4cec749379df90e895091c3544
Nathan Pearson <npearson72@gmail.com>
2022-12-26 22:18:18 -0700

    More settings

--- after/plugin/visuals.lua
+++ after/plugin/visuals.lua
@@ -1,7 +1,7 @@
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = '80'
 
-vim.cmd([[ colorscheme pencil ]])
+vim.cmd([[ colorscheme pencil ]], { silent = true, bang = true })
 
 local options = {
   -- General
